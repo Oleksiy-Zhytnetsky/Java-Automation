dde17d139570529495212e44b957ace401d5e649
Oleksiy Zhytnetsky <oleksii.zhytnetsky@gmail.com>
2025-06-13 13:00:11 +0300

    [L3] Add custom gradle plugins

--- L3/Message-Encoder-Decoder/build.gradle.kts
+++ L3/Message-Encoder-Decoder/build.gradle.kts
@@ -17,3 +17,29 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+// Plugins
+subprojects {
+    tasks.register("showProjectInfo") {
+        group = "help"
+        description = "Prints general information about the project"
+
+        doLast {
+            println("-----------------------------------------")
+            println(" Project Path: ${project.path}")
+            println("         Name: ${project.name}")
+            println("        Group: ${project.group}")
+            println("      Version: ${project.version}")
+            println("  Java version: ${JavaVersion.current()}")
+            println("-----------------------------------------")
+        }
+    }
+}
+
+tasks.register("showAllProjectsInfo") {
+    group = "help"
+    description = "Prints info for every subproject"
+    dependsOn(subprojects.map {
+        it.tasks.named("showProjectInfo")
+    })
+}
