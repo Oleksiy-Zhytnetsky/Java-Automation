f3aadc4657e8be83210ab8602dcca592cc8bd1ff
Oleksiy Zhytnetsky <oleksii.zhytnetsky@gmail.com>
2025-06-26 03:19:14 +0300

    [L6] Copy L5 project files into L6

---
+++ L6/CI-CD/src/main/java/ua/edu/ukma/Zhytnetsky/MathUtils.java
@@ -0,0 +1,23 @@
+package ua.edu.ukma.Zhytnetsky;
+
+public final class MathUtils {
+
+    private MathUtils() {}
+
+    public static Integer add(final Integer first, final Integer second) {
+        return first + second;
+    }
+
+    public static Integer mult(final Integer first, final Integer second) {
+        return first * second;
+    }
+
+    public static Integer sub(final Integer first, final Integer second) {
+        return first - second;
+    }
+
+    public static Integer div(final Integer first, final Integer second) {
+        return first / second;
+    }
+
+}
